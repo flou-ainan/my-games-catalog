d2cf2bf8e152557cce115f47cef47d412576b9af
flouainan <flouainan@gmail.com>
2023-08-02 00:41:59 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# My Games Catalog
+This project is based on Scrimba React Course AirBnb clone from the 2nd module.
+
+The objective here is to put in practice everything that I've learned on this module on a "real world way".
+
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>My games catalog</h1>
+      <p><a href="https://github.com/flou-ainan/my-games-catalog">Access the project ➡️</a></p>
+      <footer>Made by: <a href="https://github.com/flou-ainan">Flou Ainan</a></footer>
+    </>
+  )
+}
+
+export default App
